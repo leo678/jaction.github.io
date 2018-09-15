3b28cdd02b97cfd72a06c1273406ccbc838025d4
liushuai <liushuai@vtstar.net>
2018-09-15 18:03:50 +0800

    修改主页

---
+++ index/js/nav.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('.nav-item>a').on('click',
+    function() {
+        if (!$('.nav').hasClass('nav-mini')) {
+            if ($(this).next().css('display') == "none") {
+                $('.nav-item').children('ul').slideUp(300);
+                $(this).next('ul').slideDown(300);
+                $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
+            } else {
+                $(this).next('ul').slideUp(300);
+                $('.nav-item.nav-show').removeClass('nav-show');
+            }
+        }
+    });
+    $('#mini').on('click',
+    function() {
+        if (!$('.nav').hasClass('nav-mini')) {
+            $('.nav-item.nav-show').removeClass('nav-show');
+            $('.nav-item').children('ul').removeAttr('style');
+            $('.mainDiv').addClass('max-mainDiv');
+            $('.nav').addClass('nav-mini');
+        } else {
+           $(".mainDiv").removeClass('max-mainDiv');
+           $('.nav').removeClass('nav-mini');
+        }
+    });
+});
